b4db6bf44982a4ec0a7ded9a8b09da5d275f5681
krissme28 <130139109+krissme28@users.noreply.github.com>
2023-04-07 16:47:40 +0300

    task1

--- solution.py
+++ solution.py
@@ -1,11 +1,15 @@
 import pandas as pd
 import numpy as np
+import math
 
-
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 317456038 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return x.mean() # Ваш ответ
+    x = x - 139
+    log_x = np.log(x)
+    mu = np.mean(log_x)
+    
+    return mu # Ваш ответ
